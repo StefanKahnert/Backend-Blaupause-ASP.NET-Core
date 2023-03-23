78ab003db1040a2186267b4d0194888adf7b719f
Stefan Kahnert <stefan.kahnert@gmx.net>
2023-03-23 18:31:05 +0100

    Changed Unit Tests to NUnit

---
+++ Backend_Blaupause_Unit_Test/Mocks/UserAuthenticationMock.cs
@@ -0,0 +1,18 @@
+﻿using Backend_Blaupause.Helper;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_Blaupause_Unit_Test.Mocks
+{
+    public class UserAuthenticationMock
+    {
+        public static Mock<UserAuthentication> getInstance()
+        {
+            return new Mock<UserAuthentication>();
+        }
+    }
+}
