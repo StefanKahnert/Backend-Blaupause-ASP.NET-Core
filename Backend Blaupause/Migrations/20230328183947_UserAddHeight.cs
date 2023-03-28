6f2e92f51f562c13abd6561e39671e860af39996
Stefan Kahnert <stefan.kahnert@gmx.net>
2023-03-28 20:47:00 +0200

    Added Database Migration

---
+++ Backend Blaupause/Migrations/20230328183947_UserAddHeight.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_Blaupause.Migrations
+{
+    public partial class UserAddHeight : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(name: "Height", table: "User", nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Height", table: "User");
+        }
+    }
+}
